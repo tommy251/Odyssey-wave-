d9cb24f99b7b8187b74993acc6ba05792b818155
Tomiwa Babatunde <tommybab7@gmail.com>
2026-05-01 04:24:36 -0700

    fix: force add all missing src files

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
-import { getRouter } from './routes/router'
+import { getRouter } from './router'
 import './routes/styles.css'
 
 const router = getRouter()
